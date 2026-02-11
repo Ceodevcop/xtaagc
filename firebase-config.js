989d93eef8f10e5f0af36bf3f481a44757447db3
Ahmad Hamza <priahmz@gmail.com>
2026-02-11 08:11:07 +0100

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-functions.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI1MrcP977UgRLQoePxl64JOSyj9v4WpI",
+  authDomain: "xtaagc.firebaseapp.com",
+  projectId: "xtaagc",
+  storageBucket: "xtaagc.firebasestorage.app",
+  messagingSenderId: "256073982437",
+  appId: "1:256073982437:web:8da63bb7acc86c0ca98f0c"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const functions = getFunctions(app);
